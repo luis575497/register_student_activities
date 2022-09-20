3b0706f462d7299f54efcfa063cab1c7207885df
Luis Enrique Lescnao <luis575497@gmail.com>
2022-09-20 17:30:13 -0500

    Agregado poetry y prehooks al proyecto

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
 from datetime import timedelta
 
+
 class Config(object):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:admin@localhost/estudiantes'
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@localhost/estudiantes"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = True
     DEBUG = True
-    FLASK_DEBUG = 'development'
-    SECRET_KEY = 'sdfksdj156165gnsdfnsdonfs'
-    TEMPLATES_FOLDER = 'templates'
-    PERMANENT_SESSION_LIFETIME =  timedelta(minutes=5)
+    FLASK_DEBUG = "development"
+    SECRET_KEY = "sdfksdj156165gnsdfnsdonfs"
+    TEMPLATES_FOLDER = "templates"
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
+
 
 class ProductionConfig(Config):
     DEBUG = False
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
-
-    
